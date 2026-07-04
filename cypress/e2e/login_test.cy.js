cb5186c2ed2fd01eb813d7c31591e1002ad9a7f9
KWB00 <beejaykaz@gmail.com>
2026-07-04 20:12:45 +0100

    final fix maybe

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -8,6 +8,7 @@ describe('saucedemo login tests', () => {
         cy.get('[data-test="username"]').type('locked_out_user');
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
+        
         cy.url().should('include', ('/inventory'));
     });
 
